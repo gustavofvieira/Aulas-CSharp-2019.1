c2c2a4d3c51066ad510b36d0354bce5548cc0e61
Andrey Evyk <andreyevyk13@gmail.com>
2019-02-14 11:25:02 -0300

    Calculando IMC

--- Semana 2/Atividade 2/calc_imc.js
+++ Semana 2/Atividade 2/calc_imc.js
@@ -11,4 +11,8 @@ function calcular_imc() {
 	}
 	document.getElementById("resultado").innerHTML = "Ola "+nome+"\naltura: "+altura+"\npeso:"+peso+"\nIMC: "+imc;
 	
+	var frutas = document.getElementsByName("frutas");
+	for(var i = frutas.length -1 ; i>=0 ; i--){
+		document.getElementById("resultado").innerHTML += frutas[i]; 
+	}
 }
