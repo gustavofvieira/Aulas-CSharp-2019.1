139ce20ea304934fc980c34063bc106fd413ebaa
Andrey Evyk <andreyevyk13@gmail.com>
2019-02-14 10:49:56 -0300

    Calculando IMC

--- Semana 2/Atividade 2/calc_imc.js
+++ Semana 2/Atividade 2/calc_imc.js
@@ -1,1 +1,14 @@
-alert("teste");
+function saudar() {
+	var nome = document.getElementById("nome").value;
+	var altura = document.getElementById("altura").value;
+	var peso = document.getElementById("peso").value;
+	var result = altura * 2;
+	var imc = peso / result;
+	alert("Ola "+nome+"\naltura: "+altura+"\npeso:"+peso+"\nIMC: "+imc);
+}
+function calcular_imc() {
+	var altura = document.getElementById("altura").value;
+	var peso = document.getElementById("peso").value;
+	var imc = peso / altura * 2;
+
+}
