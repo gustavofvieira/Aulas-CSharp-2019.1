d9b21574d4e763c8c154232cee58c6d353428204
Gustavo Vieira <gustavofvieira@live.com>
2019-03-07 10:05:57 -0300

    C#

---
+++ C#/Escola/Escola/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola
+{
+    class Aluno
+    {
+        private String nome_completo { get; set; }
+        private int idade { get; set; }
+
+    }
+}
---
+++ C#/Primeiro_Projeto/Primeiro_Projeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Pessoas");
+        }
+    }
+}
