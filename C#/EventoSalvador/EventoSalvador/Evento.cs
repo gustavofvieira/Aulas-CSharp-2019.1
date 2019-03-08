4c4bfdaedd7c756b9e1d86d1b742a380d3643229
gustavo vieira <gustavofvieira@live.com>
2019-03-08 08:53:48 -0300

    Subindo herança

---
+++ C#/EventoSalvador/EventoSalvador/Evento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventoSalvador
+{
+    class Evento
+    {
+        public string Nome { get; set; }
+        public string Local { get; set; }
+        public string DataHora { get; set; }
+        public int Classificacao { get; set; }
+
+        public bool autorizar(int idade)
+        {
+            if (idade >= Classificacao)// como se tivesse pegando o getClassificacao
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ C#/EventoSalvador/EventoSalvador/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventoSalvador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publico eventoPublico = new Publico();
+            eventoPublico.Classificacao = 18;
+            bool x = eventoPublico.autorizar(18);
+            if (x)
+            {
+                Console.WriteLine("Liberado!");
+            }
+            else {
+                Console.WriteLine("Negado!");
+            }
+        }
+    }
+}
