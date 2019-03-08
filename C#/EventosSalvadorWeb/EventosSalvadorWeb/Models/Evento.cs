5500fc67a12b7fb6568445e95f7b741e191b220d
gustavo vieira <gustavofvieira@live.com>
2019-03-08 11:47:51 -0300

    subindo Web

---
+++ C#/EventosSalvadorWeb/EventosSalvadorWeb/Models/Evento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventosSalvadorWeb.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Nome { get; set; }
+        public string Data { get; set; }
+        public int Classificacao { get; set; }
+    }
+}
---
+++ C#/EventosSalvadorWeb/EventosSalvadorWeb/Models/EventosSalvadorWebContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EventosSalvadorWeb.Models
+{
+    public class EventosSalvadorWebContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public EventosSalvadorWebContext() : base("name=EventosSalvadorWebContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<EventosSalvadorWeb.Models.Evento> Eventoes { get; set; }
+    }
+}
