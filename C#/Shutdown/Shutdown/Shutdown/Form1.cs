f42083a21aa3e8f72cc01d06961b02c93ec17ae9
gustavo vieira <gustavofvieira@live.com>
2019-03-08 09:48:59 -0300

    Subindo tentativa de shutdown

---
+++ C#/Shutdown/Shutdown/Shutdown/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace Shutdown
+{
+    public partial class Form1 : Form
+    {
+        //[System.Runtime.InteropServices.DllImport("user32")]
+        [DllImport("user32")]
+        public static extern void Lockworkstation(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("shutdown", "/s /t 6");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("shutdown", "/a");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Lockworkstation();
+        }
+    }
+}
