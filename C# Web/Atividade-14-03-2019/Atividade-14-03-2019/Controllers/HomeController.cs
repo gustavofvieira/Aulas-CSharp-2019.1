76e2fd5686e9d1e54c32a992a9565f159b5b91ae
Gustavo Ferreira <gustavofvieira@live.com>
2019-03-15 10:07:35 -0300

    Atividade 14-03-2019

--- C# Web/Atividade-14-03-2019/Atividade-14-03-2019/Controllers/HomeController.cs
+++ C# Web/Atividade-14-03-2019/Atividade-14-03-2019/Controllers/HomeController.cs
@@ -60,7 +60,7 @@ namespace Atividade_14_03_2019.Controllers
             ViewBag.Quadro = "Quadro Bonito!.";
             List<Quadro> quadros = new List<Quadro>();
             Quadro quadro1 = new Quadro();
-            //quadro1.Autor(a1);
+            //quadro1.Autor
             quadro1.AnoLancamento = 2019;
             quadro1.Descricao = "Quadro de rede";
 
@@ -78,8 +78,35 @@ namespace Atividade_14_03_2019.Controllers
             quadros.Add(quadro2);
             quadros.Add(quadro3);
 
-            ViewBag.Quadros = quadros;// passando o Array pra viewbag
+            
+            List<Autor> autores = new List<Autor>();
 
+            Autor a1 = new Autor();
+            a1.Nome = "Gustavo";
+            a1.Cpf = "000";
+            a1.Aquarela = true;
+            //a1.PintaAquarela(true);
+            Autor a2 = new Autor();
+            a2.Nome = "Gildete";
+            a2.Cpf = "111";
+            a2.Aquarela = false;
+            //a2.PintaAquarela(false);
+            Autor a3 = new Autor();
+            a3.Nome = "Thiago";
+            a3.Cpf = "222";
+            a3.Aquarela = true;
+            //a3.PintaAquarela(true);
+
+            autores.Add(a1);
+            autores.Add(a2);
+            autores.Add(a3);
+
+            quadro1.Autor = a1;
+            quadro2.Autor = a2;
+            quadro3.Autor = a3;
+
+            ViewBag.Autores = autores;
+            ViewBag.Quadros = quadros;// passando o Array pra viewbag
             return View();
         }
     }
